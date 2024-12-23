3bb8040059524cb400c3df561b8b3e87e180fdb8
Hong Youl, YUN <hongyoul03@gmail.com>
2024-12-23 20:34:42 +0900

    상품리스트 역정렬

--- src/components/prodcutList/CompPrdList.js
+++ src/components/prodcutList/CompPrdList.js
@@ -26,6 +26,7 @@ const CompPrdList = () => {
       try {
         const response = await axios.get(`${host}/product/list`);
         const products = response.data;
+        products.sort((a, b) => b.pdNo - a.pdNo);
         setPrdList(products);
 
         const uniqueDepth1 = [...new Set(products.map((prd) => prd.depth1))];
